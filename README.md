099604a77d977f23ed70cca7f95ec5cb26ffe4d0
MrSterdy <mrsterdy@gmail.com>
2023-11-02 01:55:22 +0300

    no comments

---
+++ README.md
@@ -0,0 +1,15 @@
+# etherscan-enot
+
+## Локальный запуск
+
+Установка зависимостей
+
+```bash
+poetry install
+```
+
+и запуск
+
+```bash
+poetry run start
+```
--- src/etherscan-enot/main.py
+++ src/etherscan-enot/main.py
@@ -19,9 +19,12 @@ def generate_hash():
 
 
 async def wait_for_captcha(page):
+    # Так как капча от гугла находится в iframe, то её загрузка может занять некоторое время
     await page.waitFor('iframe[title="reCAPTCHA"]')
+    # Капчи от гугла всегда имеют префикс "a-" перед названием
     captcha_frame = [x for x in page.frames if x.name.startswith("a-")][0]
-    await captcha_frame.waitFor('div.recaptcha-checkbox-border')
+    await captcha_frame.waitFor('div.recaptcha-checkbox-border', {'timeout': 0})
+    await captcha_frame.click('div.recaptcha-checkbox-border')
 
     while True:
         checkbox_checked = await captcha_frame.evaluate('''() => {
@@ -41,6 +44,7 @@ def submit_form(page):
 
 
 async def wait_for_email_verification(tmp: TempMail, inbox):
+    # Проверяем почту на новые письма каждую секунду
     while True:
         emails = tmp.getEmails(inbox)
         if len(emails) != 0:
@@ -56,12 +60,15 @@ async def get_api_key():
     username = generate_hash()
     password = generate_hash()
 
+    # Заходим на сайт регистрации
     browser = await pyppeteer.launch({'headless': False})
     register_page = await browser.newPage()
     await register_page.goto('https://etherscan.io/register')
 
+    # Ждём загрузку капчи и её решения пользователем
     await wait_for_captcha(register_page)
 
+    # Заполнение формы
     await register_page.type('#ContentPlaceHolder1_txtUserName', username)
     await register_page.type('#ContentPlaceHolder1_txtEmail', inbox.address)
     await register_page.type('#ContentPlaceHolder1_txtConfirmEmail', inbox.address)
@@ -76,7 +83,8 @@ async def get_api_key():
     await verification_page.waitFor('a[href="/login"]')
 
     login_page = await browser.newPage()
-    await login_page.goto('https://etherscan.io/login')
+    # Вот тут то вступает в игру CloudFlare
+    await login_page.goto('https://etherscan.io/login', {'timeout': 0})
     await wait_for_captcha(login_page)
 
     await login_page.type('#ContentPlaceHolder1_txtUserName', username)
@@ -92,11 +100,14 @@ async def get_api_key():
     await api_page.click('#ContentPlaceHolder1_addnew')
     await api_page.waitFor('#ContentPlaceHolder1_txtAppName')
     await api_page.type('#ContentPlaceHolder1_txtAppName', generate_hash())
-    await api_page.click('#ContentPlaceHolder1_btnSubmit')
+
+    await submit_form(api_page)
+
     await api_page.waitForNavigation()
 
     content = await api_page.content()
 
+    # Ключ имеет вид <span id="a_[КЛЮЧ]">[КЛЮЧ]</span>
     return re.findall(r'id="a_(.*)"', content)[0]
 
 
@@ -107,6 +118,10 @@ async def scrap():
     f.close()
 
 
-if __name__ == '__main__':
+def main():
     loop = asyncio.new_event_loop()
     loop.run_until_complete(scrap())
+
+
+if __name__ == '__main__':
+    main()
